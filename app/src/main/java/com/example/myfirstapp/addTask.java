135b50e719a7a10fd6bf2b0b6e693c1b7da02c9d
ASEEL ALBANNA <ASEELNIHAD.AN@GMAIL.COM>
2021-05-16 15:58:54 +0300

    edit

--- app/src/main/java/com/example/myfirstapp/addTask.java
+++ app/src/main/java/com/example/myfirstapp/addTask.java
@@ -1,8 +1,11 @@
 package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,7 +19,15 @@ public class addTask extends AppCompatActivity {
         setContentView(R.layout.activity_add_task);
         submit = findViewById(R.id.textView7);
         submit.setVisibility(View.INVISIBLE);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent back = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(back);
+        return true;
+        }
 
     public void submit(View view) {
         submit.setVisibility(View.VISIBLE);
